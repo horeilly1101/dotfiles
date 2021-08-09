2cf8fe9c5c7ea9d61bfccaf0f75d0e9db0879d97
Hugh O'Reilly <horeilly1101@gmail.com>
2021-08-09 11:19:40 -0700

    add zsh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@
 dotfiles=(.*)
 for f in ${dotfiles[*]}; do
     if [ ${#f} -gt 2 ] && [ "$f" !=  ".git" ]; then
-        rm ~/$f
+        # Remove the file if it exists. 
+        if [ -e ~/$f ]; then
+            rm ~/$f
+        fi
+        # Create a symbolic link to the file in the home directory.
         ln -sv $(pwd)/$f ~
     fi
 done
