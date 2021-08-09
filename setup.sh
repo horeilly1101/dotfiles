fd085c5ae22a8ccec8ec844c1ee8c1048ec19f27
Hugh O'Reilly <horeilly1101@gmail.com>
2021-08-09 11:45:20 -0700

    update comment

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Create sylinks to dotfiles in the home directory.
+# Create symlinks to dotfiles in the home directory.
 dotfiles=(.*)
 for f in ${dotfiles[*]}; do
     if [ ${#f} -gt 2 ] && [ "$f" !=  ".git" ]; then
@@ -8,7 +8,7 @@ for f in ${dotfiles[*]}; do
         if [ -e ~/$f ]; then
             rm ~/$f
         fi
-        # Create a symbolic link to the file in the home directory.
+        # Create a symlink to the file in the home directory.
         ln -sv $(pwd)/$f ~
     fi
 done
